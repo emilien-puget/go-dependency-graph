134081df9549f5aa73a0195f7d76adc6fbd9afb6
Emilien Puget <puget.e@pm.me>
2023-07-05 16:37:18 +0200

    ordered map

--- pkg/generator/mermaid/class.go
+++ pkg/generator/mermaid/class.go
@@ -23,12 +23,7 @@ func GenerateClassFromSchema(writer *bufio.Writer, s parse.AstSchema) error {
 	var classBuf bytes.Buffer
 	var relationBuf bytes.Buffer
 
-	keys := make([]string, 0, len(s.Packages))
-	for k := range s.Packages {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
-	for _, k := range keys {
+	for _, k := range orderedKeys(s.Packages) {
 		err := handlePackages(&classBuf, &relationBuf, k, s.Packages[k])
 		if err != nil {
 			return err
@@ -55,12 +50,7 @@ func handlePackages(classBuf, relationBuf *bytes.Buffer, packageName string, ser
 		return err
 	}
 
-	keys := make([]string, 0, len(services))
-	for k := range services {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
-	for _, serviceName := range keys {
+	for _, serviceName := range orderedKeys(services) {
 		service := services[serviceName]
 		err := handleService(classBuf, relationBuf, packageName, serviceName, &service)
 		if err != nil {
@@ -72,9 +62,9 @@ func handlePackages(classBuf, relationBuf *bytes.Buffer, packageName string, ser
 }
 
 func handleService(classBuf, relationBuf *bytes.Buffer, packageName, serviceName string, service *parse.Dependency) error {
-	fqdn := packageName + packageSeparator + serviceName
+	serviceFqdn := packageName + packageSeparator + serviceName
 
-	_, err := fmt.Fprintf(classBuf, "class `%s` {\n", fqdn)
+	_, err := fmt.Fprintf(classBuf, "class `%s` {\n", serviceFqdn)
 	if err != nil {
 		return err
 	}
@@ -84,29 +74,46 @@ func handleService(classBuf, relationBuf *bytes.Buffer, packageName, serviceName
 	}
 	classBuf.WriteString("}\n\n")
 
-	for _, deps := range service.Deps {
-		sort.Slice(deps, func(i, j int) bool {
-			return deps[i].PackageName+deps[i].DependencyName < deps[j].PackageName+deps[j].DependencyName
-		})
-		for _, d := range deps {
-			s := d.PackageName + packageSeparator + d.DependencyName
-			if len(d.Funcs) != 0 {
-				sort.Slice(d.Funcs, func(i, j int) bool {
-					return d.Funcs[i] < d.Funcs[j]
-				})
-				for _, fn := range d.Funcs {
-					_, err := fmt.Fprintf(relationBuf, "`%s` ..> `%s`: %s\n", fqdn, s, fn)
-					if err != nil {
-						return err
-					}
-				}
-			} else {
-				_, err := fmt.Fprintf(relationBuf, "`%s` ..> `%s`\n", fqdn, s)
+	for _, d := range orderedKeys(service.Deps) {
+		err := handleDeps(service.Deps[d], relationBuf, serviceFqdn)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func handleDeps(deps []parse.Dep, relationBuf *bytes.Buffer, serviceFqdn string) error {
+	sort.SliceStable(deps, func(i, j int) bool {
+		return deps[i].PackageName+deps[i].DependencyName < deps[j].PackageName+deps[j].DependencyName
+	})
+	for _, d := range deps {
+		s := d.PackageName + packageSeparator + d.DependencyName
+		if len(d.Funcs) != 0 {
+			sort.Slice(d.Funcs, func(i, j int) bool {
+				return d.Funcs[i] < d.Funcs[j]
+			})
+			for _, fn := range d.Funcs {
+				_, err := fmt.Fprintf(relationBuf, "`%s` ..> `%s`: %s\n", serviceFqdn, s, fn)
 				if err != nil {
 					return err
 				}
 			}
+		} else {
+			_, err := fmt.Fprintf(relationBuf, "`%s` ..> `%s`\n", serviceFqdn, s)
+			if err != nil {
+				return err
+			}
 		}
 	}
 	return nil
+}
+
+func orderedKeys[v any](tab map[string]v) []string {
+	keys := make([]string, 0, len(tab))
+	for k := range tab {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return keys
 }
